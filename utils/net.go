e3cc6251f3b44e1a7a86f18d8496e50b03a76bc5
Daniel <reymonsma@gmail.com>
2025-08-31 18:19:59 +0200

    feat: add body utils & validation

---
+++ utils/net.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+    "banking/validation"
+    "encoding/json"
+)
+
+// Decodes an http body and validates it.
+//
+// If the decoding process fails OR the body is invalid,
+// automatically sends a 400-status response
+//
+// You must check the boolean flag that's returned to learn
+// if the response has been sent (decoding failed)
+//
+// Example:
+//
+// body, ok := DecodeBody[MyBody](w, req)
+// if !ok {
+//     return
+// }
+// Do something with the body, etc.
+func DecodeBody[T validation.Validator](w http.ResponseWriter, req *http.Request) (T, error) {
+    var v T
+	if err := json.NewDecoder(req.Body).Decode(&v); err != nil {
+        return v, err
+	}
+	if problems := v.Valid(); len(problems) > 0 {
+        return v, fmt.Errorf("invalid payload")
+	}
+	return v, nil
+}
+
+// JSON-Encodes a body and automatically writes its contents to the response
+// If en error occurs, closes the connection and returns the error to the function user
+// The user must not call any actions on the response writer afterwards
+func EncodeBody[T any](w http.ResponseWriter, status int, v T) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return fmt.Errorf("encode json: %w", err)
+	}
+	return nil
+}
+
---
+++ validation/validation.go
@@ -0,0 +1,39 @@
+package validation
+
+import (
+    "fmt"
+)
+
+type Validator interface {
+    // Validates an object and returns an error map
+    // If len(map) > 0, the object is invalid
+    Valid() map[string]string
+}
+
+type err struct {}
+
+var Err err
+
+func (err) Min(n int) string {
+    return fmt.Sprintf("shouldn't be less than %d", n)
+}
+func (err) Max(n int) string {
+    return fmt.Sprintf("shouldn't be greater than %d", n)
+}
+func (err) Required() string {
+    return "required field"
+}
+func (err) InvalidEmail() string {
+    return "invalid email"
+}
+
+type valid struct{}
+
+var Valid valid
+
+func (valid) Email(v string) bool {
+    _ = v
+    return true
+}
+
+
